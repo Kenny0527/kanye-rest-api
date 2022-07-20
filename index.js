806c22859aaa6e338efef9baea05565f6a1bb47b
Kramclam <kennethramclam@gmail.com>
2022-07-20 18:04:20 -0400

    updated with npm

---
+++ index.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+import express from "express";
+import cors from "cors";
+
+// THIS IS AN APP/SERVER
+const app = express();
+const URL = "https://api.kanye.rest";
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`server listening on port: ${PORT}`);
+});
+
+app.use(cors());
+
+// <SERVERNAME>.<METHODNAME>(<PATHNAME>, <CALLBACK FUNCTION>)
+app.get("/kanye_quotes", async (req, res) => {
+  try {
+    const response = await axios.get(URL);
+    res.send(response.data);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//   axios
+//     .get(URL)
+//     .then((response) => {
+//       console.log(response.data);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+//
